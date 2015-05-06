59be6bdda6282b7873a17cda76e01e84e13227b8
Marco <marcocatta@gmail.com>
2015-05-06 15:16:52 +0200

    First commit

---
+++ jquery.videohandler.js
@@ -0,0 +1,37 @@
+(function($) {
+	$.fn.VideoHandler = function(options) {
+
+		this.el = this[0];
+
+		/**
+		 * Functions
+		 */
+
+		this.play = function() {
+			this.el.play();
+		}
+
+		this.pause = function() {
+			this.el.pause();
+		}
+
+		/**
+		 * Events
+		 */
+
+		if(options.onReady != undefined)
+			this.el.addEventListener('canplay', options.onReady);
+
+		if(options.onComplete != undefined)
+			this.el.addEventListener('ended', options.onComplete);
+
+		if(options.onError != undefined)
+			this.el.addEventListener('error', options.onError);
+
+		if(options.onPause != undefined)
+			this.el.addEventListener('pause', options.onPause);
+
+		return this;
+	}
+
+})(jQuery);
